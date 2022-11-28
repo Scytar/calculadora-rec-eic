b3ae05cdc9e4e9cbec4366ac810c63cb1973faae
Scytar <scytarnoob@gmail.com>
2022-11-28 19:26:14 -0300

    Created REC calculator

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
-const tr1 = document.getElementById("tr1");
-const tr2 = document.getElementById("tr2");
-const av1 = document.getElementById("av1");
-const av2 = document.getElementById("av2");
-const av3 = document.getElementById("av3");
-const av4 = document.getElementById("av4");
+// const tr1 = document.getElementById("tr1");
+// const tr2 = document.getElementById("tr2");
+// const av1 = document.getElementById("av1");
+// const av2 = document.getElementById("av2");
+// const av3 = document.getElementById("av3");
+// const av4 = document.getElementById("av4");
+const maInput = document.getElementById("ma");
 const button = document.getElementById('button');
 const result = document.getElementById('result');
 
-function miaucular(tr1,tr2,av1,av2,av3,av4) {
+// function miaucular(tr1,tr2,av1,av2,av3,av4) {
+function miaucular(ma) {
     try {
-        const media = tr1 + tr2 + ((3*av1 + 3*av2 + 3*av3 + av4)/10)
-        console.log('media: ',media)
+        // const media = tr1 + tr2 + ((3*av1 + 3*av2 + 3*av3 + av4)/10)
+        const media = (7*ma).toFixed(2)
+        console.log('media anual: ',media)
 
-        if (media>=21){
-            return "Travaldo te dá os parabéns! Você não precisa da REC"
+        if (ma>=7){
+            return "Parabéns! Você não precisa da REC"
         } else {
-            let mediaRec
-            console.log('av1: ',av1)
-            console.log('av2: ',av2)
-            if (av1<av2){
-                mediaRec = (21 - tr1 - tr2 - (((3*av1 + 3*av2 + 3*av3 + av4)/10))*(10/3))+av1;
-                console.log('(av1) mediaRec: ',mediaRec)
-            } else {
-                mediaRec = (21 - tr1 - tr2 - (((3*av1 + 3*av2 + 3*av3 + av4)/10))*(10/3))+av2;
-                // mediaRec = 21-(tr1 + tr2 + ((3*av1 + 3*av2 + 3*av3 + av4)/10)*(10/3))+av2;
-                console.log('(av2) mediaRec: ',mediaRec)
-            }
+            let mediaRec = (50-media)/3
+            console.log('media Rec: ',mediaRec)
+            mediaRec = Math.ceil(mediaRec*100)/100
+            console.log('new media Rec: ',mediaRec)
+            // let mediaRec
+            // console.log('av1: ',av1)
+            // console.log('av2: ',av2)
+            // if (av1<av2){
+            //     mediaRec = (21 - tr1 - tr2 - (((3*av1 + 3*av2 + 3*av3 + av4)/10))*(10/3))+av1;
+            //     console.log('(av1) mediaRec: ',mediaRec)
+            // } else {
+            //     mediaRec = (21 - tr1 - tr2 - (((3*av1 + 3*av2 + 3*av3 + av4)/10))*(10/3))+av2;
+            //     // mediaRec = 21-(tr1 + tr2 + ((3*av1 + 3*av2 + 3*av3 + av4)/10)*(10/3))+av2;
+            //     console.log('(av2) mediaRec: ',mediaRec)
+            // }
 
-            return "Você precisa de "+(mediaRec)
+            return "Você precisa de "+(mediaRec.toFixed(2))
         }
     } catch (error) {
         return error
@@ -39,9 +46,10 @@ function miaucular(tr1,tr2,av1,av2,av3,av4) {
 
 // (21 - tr1 - tr2 - ((3*av1 + 3*av2 + 3*av3 + av4)/10))*(10/3)+MIN( av1 : av2)
 
-button.addEventListener('click',(e)=>{
-    e.preventDefault;
-    e.stopPropagation;
+maInput.addEventListener('input',(e)=>{
+    // e.preventDefault;
+    // e.stopPropagation;
     e.stopImmediatePropagation;
-    result.textContent = miaucular(parseFloat(tr1.value),parseFloat(tr2.value),parseFloat(av1.value),parseFloat(av2.value),parseFloat(av3.value),parseFloat(av4.value));
+    //result.textContent = miaucular(parseFloat(tr1.value),parseFloat(tr2.value),parseFloat(av1.value),parseFloat(av2.value),parseFloat(av3.value),parseFloat(av4.value));
+    result.textContent = miaucular(maInput.value)
 })
